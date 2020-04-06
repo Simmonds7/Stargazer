18ce475b2563343feb74cd47cea9d59d44721332
tranqnhan <nt305748@gmail.com>
2020-04-05 23:20:56 -0500

    sometimes i want to cry
    * Decouple Player
    * Redo some stuff

---
+++ Stargazer/Assets/Scripts/Actions/TimeControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TimeControl : MonoBehaviour
+{
+
+    public float slowdownFactor = 0.2f;
+    public float slowdownLength = 3f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            DoSlowmotion();
+        }
+        else
+        {
+            Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        }
+    }
+
+    public void DoSlowmotion()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+
+    }
+
+}
--- Stargazer/Assets/Scripts/Entities/BotAI.cs
+++ Stargazer/Assets/Scripts/Entities/BotAI.cs
@@ -2,162 +2,201 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BotAI : Entity {
+public class BotAI : Entity
+{
 
-	public GameObject weaponPrefab;
-	private Transform centerTransform;
-	public GameObject deathExplosion;
+    public GameObject weaponPrefab;
+    private Transform centerTransform;
+    public GameObject deathExplosion;
 
-	[SerializeField]
-	private Transform targetRef;
-	private bool targetInLineOfSight;
-	private bool targetDetected;
-	private bool targetInShootingRange;
-	private bool targetCanBeSeen;
+    [SerializeField]
+    private Transform targetRef;
+    private bool targetInLineOfSight;
+    private bool targetDetected;
+    private bool targetInShootingRange;
+    private bool targetCanBeSeen;
 
-	public int detectionRange = 60;
-	public int shootingRange = 80;
+    public int detectionRange = 60;
+    public int shootingRange = 80;
 
-	private MovementAI movementAI;
-	private EquipAction equipAction;
+    private MovementAI movementAI;
+    private EquipAction equipAction;
 
-	void Start () {
-		centerTransform = transform.GetChild(0);
-		centerTransform.rotation = transform.rotation;
-		this.direction = this.transform.eulerAngles;
-		this.position = this.transform.position;
-		this.currentSpeed = 0.0f;
+    void Start()
+    {
+        centerTransform = transform.GetChild(0);
+        centerTransform.rotation = transform.rotation;
+        this.direction = this.transform.eulerAngles;
+        this.position = this.transform.position;
+        this.currentSpeed = 0.0f;
 
-		targetDetected = targetCanBeSeen = targetInLineOfSight = targetInShootingRange = false;
+        targetDetected = targetCanBeSeen = targetInLineOfSight = targetInShootingRange = false;
 
-		movementAI = GetComponent<MovementAI>();
-		Equipment weapon = Instantiate(weaponPrefab, Vector3.zero, Quaternion.Euler(0,0,0)).GetComponent<Equipment>();
+        movementAI = GetComponent<MovementAI>();
+        GameObject weaponObject = (GameObject)Instantiate(weaponPrefab);
+        RangeWeapon weapon = weaponObject.GetComponent<RangeWeapon>();
 
-		equipAction = GetComponent<EquipAction>();
-		equipAction.OnEquip(weapon, this.centerTransform);
-	}
+        equipAction = GetComponent<EquipAction>();
+        equipAction.EquipItem(weapon, this.centerTransform);
+    }
 
-	void CheckStatus() {
+    void CheckStatus()
+    {
 
-		Vector3 dir =  targetRef.position - centerTransform.position;
-		RaycastHit hitInfoCenter;
-		float distApart = .3f;
-		float x;
-		float z;
-		float radians = Mathf.Atan2(dir.z, dir.x);
+        Vector3 dir = targetRef.position - centerTransform.position;
+        RaycastHit hitInfoCenter;
+        float distApart = .3f;
+        float x;
+        float z;
+        float radians = Mathf.Atan2(dir.z, dir.x);
 
-		RaycastHit hitInfoLeft;
-		x = distApart * Mathf.Cos(radians + Mathf.PI / 2);
-		z = distApart * Mathf.Sin(radians + Mathf.PI / 2);
-		Ray raySpreadLeft = new Ray (position + new Vector3(x, 0, z), dir);
-		bool left = Physics.Raycast(raySpreadLeft, out hitInfoLeft);
+        RaycastHit hitInfoLeft;
+        x = distApart * Mathf.Cos(radians + Mathf.PI / 2);
+        z = distApart * Mathf.Sin(radians + Mathf.PI / 2);
+        Ray raySpreadLeft = new Ray(position + new Vector3(x, 0, z), dir);
+        bool left = Physics.Raycast(raySpreadLeft, out hitInfoLeft);
 
-		RaycastHit hitInfoRight;
-		x = distApart * Mathf.Cos(radians - Mathf.PI / 2);
-		z = distApart * Mathf.Sin(radians - Mathf.PI / 2);
-		Ray raySpreadRight = new Ray (position + new Vector3(x, 0, z), dir);
-		bool right = Physics.Raycast(raySpreadRight, out hitInfoRight);
+        RaycastHit hitInfoRight;
+        x = distApart * Mathf.Cos(radians - Mathf.PI / 2);
+        z = distApart * Mathf.Sin(radians - Mathf.PI / 2);
+        Ray raySpreadRight = new Ray(position + new Vector3(x, 0, z), dir);
+        bool right = Physics.Raycast(raySpreadRight, out hitInfoRight);
 
-		Ray rayCanBeSeen = new Ray(position, dir);
-		bool center = Physics.Raycast(rayCanBeSeen, out hitInfoCenter);
+        Ray rayCanBeSeen = new Ray(position, dir);
+        bool center = Physics.Raycast(rayCanBeSeen, out hitInfoCenter);
 
-		if (left &&right && center) {
-			if (hitInfoCenter.transform.tag == "Player" && 
-				hitInfoLeft.transform.tag == "Player" && 
-				hitInfoRight.transform.tag == "Player") {
-				targetCanBeSeen = true;
-			} else {
-				targetCanBeSeen = false;
-			}
-		} else {
-			targetCanBeSeen = false;
-		}
-			
+        if (left && right && center)
+        {
+            if (hitInfoCenter.transform.tag == "Player" &&
+                hitInfoLeft.transform.tag == "Player" &&
+                hitInfoRight.transform.tag == "Player")
+            {
+                targetCanBeSeen = true;
+            }
+            else
+            {
+                targetCanBeSeen = false;
+            }
+        }
+        else
+        {
+            targetCanBeSeen = false;
+        }
 
-		//Sends a raycast to determine if the player is infront of the enemy.
-		Ray rayLineOfSight = new Ray(centerTransform.position, centerTransform.forward);
 
-		if (Physics.Raycast(rayLineOfSight, out hitInfoCenter)) { 
-			if (hitInfoCenter.transform.tag == "Player") {
-				targetInLineOfSight = true;
-				} else {
-				targetInLineOfSight = false;
-			}
-		} else {
-			targetInLineOfSight = false;
-		}
+        //Sends a raycast to determine if the player is infront of the enemy.
+        Ray rayLineOfSight = new Ray(centerTransform.position, centerTransform.forward);
 
-		//if target is within the spotting range then activate target spotted
-		if (Vector3.Distance(targetRef.position, transform.position) >= detectionRange) {
-			targetDetected = false;
-		} else {
-			targetDetected = true;
-		}
+        if (Physics.Raycast(rayLineOfSight, out hitInfoCenter))
+        {
+            if (hitInfoCenter.transform.tag == "Player")
+            {
+                targetInLineOfSight = true;
+            }
+            else
+            {
+                targetInLineOfSight = false;
+            }
+        }
+        else
+        {
+            targetInLineOfSight = false;
+        }
 
-		if (Vector3.Distance(targetRef.position, transform.position) >= shootingRange) {
-			targetInShootingRange = false;
-		} else {
-			targetInShootingRange = true;
-		}
-	}
+        //if target is within the spotting range then activate target spotted
+        if (Vector3.Distance(targetRef.position, transform.position) >= detectionRange)
+        {
+            targetDetected = false;
+        }
+        else
+        {
+            targetDetected = true;
+        }
 
-	void AIBehavior() {
-		//If target is spotted...
-		if (targetDetected) {
-			if (movementAI != null) {
-				if (!targetInShootingRange || !targetCanBeSeen) {
-					movementAI.NavigateTo(targetRef.position);
-				} else {
-					movementAI.EndNavigation();
-				}
-			}
+        if (Vector3.Distance(targetRef.position, transform.position) >= shootingRange)
+        {
+            targetInShootingRange = false;
+        }
+        else
+        {
+            targetInShootingRange = true;
+        }
+    }
 
-			if (targetInLineOfSight)
-			{
-				this.equipment.OnActivate();
-			}
+    void AIBehavior()
+    {
+        //If target is spotted...
+        if (targetDetected)
+        {
+            if (movementAI != null)
+            {
+                if (!targetInShootingRange || !targetCanBeSeen)
+                {
+                    movementAI.NavigateTo(targetRef.position);
+                }
+                else
+                {
+                    movementAI.EndNavigation();
+                }
+            }
 
-			if (targetCanBeSeen && targetInShootingRange) {
-				Quaternion q = Quaternion.LookRotation(targetRef.position - centerTransform.position);
+            if (targetInLineOfSight)
+            {
+                if (equipments[EquipmentType.Weapon] != null)
+                {
+                    equipments[EquipmentType.Weapon].Activate();
+                }
+            }
 
-				Quaternion rot = Quaternion.Slerp(centerTransform.rotation, q, this.turnSpeed * Time.deltaTime);
+            if (targetCanBeSeen && targetInShootingRange)
+            {
+                Quaternion q = Quaternion.LookRotation(targetRef.position - centerTransform.position);
 
-				this.direction = new Vector3(0, rot.eulerAngles.y, 0);
-				centerTransform.eulerAngles = new Vector3(rot.eulerAngles.x, centerTransform.eulerAngles.y, rot.eulerAngles.z);
-			}
-		}
-	}
+                Quaternion rot = Quaternion.Slerp(centerTransform.rotation, q, this.turnSpeed * Time.deltaTime);
 
-	public override void Death() {
-		if (deathExplosion != null){
-			GameObject explode = Instantiate(deathExplosion, transform.position, transform.rotation) as GameObject;
-			explode.transform.localScale = Vector3.one * 2;
-			ParticleSystem parts = explode.GetComponent<ParticleSystem>();
-			float totalDuration = parts.main.duration + parts.main.startLifetime.constant;
-			Destroy(explode, totalDuration);
-		}
+                this.direction = new Vector3(0, rot.eulerAngles.y, 0);
+                centerTransform.eulerAngles = new Vector3(rot.eulerAngles.x, centerTransform.eulerAngles.y, rot.eulerAngles.z);
+            }
+        }
+    }
 
-		if (equipment != null) {
-			this.equipAction.OnDrop(equipment);
-		}
+    public override void Death()
+    {
+        if (deathExplosion != null)
+        {
+            GameObject explode = Instantiate(deathExplosion, transform.position, transform.rotation) as GameObject;
+            explode.transform.localScale = Vector3.one * 2;
+            ParticleSystem parts = explode.GetComponent<ParticleSystem>();
+            float totalDuration = parts.main.duration + parts.main.startLifetime.constant;
+            Destroy(explode, totalDuration);
+        }
 
-		Destroy(this.gameObject);
-	}
+        if (equipments[EquipmentType.Weapon] != null)
+        {
+            this.equipAction.UnequipItem(equipments[EquipmentType.Weapon]);
+        }
 
-	void Update() {
-		if (targetRef != null) {
-			CheckStatus();
-			AIBehavior();
-		}
-	}
+        Destroy(this.gameObject);
+    }
 
-	private void FixedUpdate() {	
-		if (this.currentSpeed != 0.0f) {
-			transform.Translate(Vector3.forward * this.currentSpeed * Time.fixedDeltaTime);
-			this.position = transform.position;
-			centerTransform.rotation = Quaternion.Slerp(centerTransform.rotation, transform.rotation, this.turnSpeed * Time.deltaTime);
-		} 
-		
-		transform.eulerAngles = this.direction;
-	}
+    void Update()
+    {
+        if (targetRef != null)
+        {
+            CheckStatus();
+            AIBehavior();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.currentSpeed != 0.0f)
+        {
+            transform.Translate(Vector3.forward * this.currentSpeed * Time.fixedDeltaTime);
+            this.position = transform.position;
+            centerTransform.rotation = Quaternion.Slerp(centerTransform.rotation, transform.rotation, this.turnSpeed * Time.deltaTime);
+        }
+
+        transform.eulerAngles = this.direction;
+    }
 }
